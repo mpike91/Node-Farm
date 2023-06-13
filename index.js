fab4b46ffe28c20dbec0bacea5c6443197a01050
Matt Pike <mattp91@gmail.com>
2023-06-13 10:46:46 -0400

    Added dependencies

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
+// Core modules
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
 
+// Third-party modules
+const slugify = require('slugify');
+
+// My own modules
 const replaceTemplate = require('./modules/replaceTemplate');
 
 /////////////////////////////////////////
@@ -36,6 +41,11 @@ const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.htm
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
+
+const slugs = dataObj.map(el => slugify(el.productName, { lower: true }));
+console.log(slugs);
+
+console.log(slugify('Fresh Avocados', { lower: true }));
 
 const server = http.createServer((req, res) => {
 
